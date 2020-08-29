5559be3df0ac356e613345e4c3f060b134f40804
András Németh <sunarchx@gmail.com>
2020-08-29 16:37:57 +0200

    Add date timestamp batch script

---
+++ batch-timestamp-by-stackoverflow/date-timestamp.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem by https://stackoverflow.com/users/2299431/foxidrive
+rem https://stackoverflow.com/questions/11037831/filename-timestamp-in-windows-cmd-batch-script-getting-truncated
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo datestamp: "%datestamp%"
+echo timestamp: "%timestamp%"
+echo fullstamp: "%fullstamp%"
+
+pause
